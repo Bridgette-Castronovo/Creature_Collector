1b796b702d76b060891dc8c91c9f862b03d81955
twest67 <63468489+Marzipayn@users.noreply.github.com>
2026-02-23 09:11:45 -0500

    Feed System

--- Assets/Scripts/CareButtons.cs
+++ Assets/Scripts/CareButtons.cs
@@ -1,12 +1,190 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using TMPro;
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine.UI;
+using System;
+using UnityEngine.Timeline;
 
 public class CareButtons : MonoBehaviour
 {
-    [SerializeField]
+    [SerializeField] private TextMeshProUGUI dayText;
+    [SerializeField] private TextMeshProUGUI animalNeeds;
+    [SerializeField] private Button FeedButton;
+    [SerializeField] private Button WaterButton;
+    [SerializeField] private Button EntertainmentButton;
+    [SerializeField] private Button HayButton;
+    [SerializeField] private Button FruitButton;
+    [SerializeField] private Button LiveFoodButton;
+    [SerializeField] public Canvas canvas;
 
-    public void FeedButton()
+    private int hunger = 50;
+    private int thirst = 50;
+    private int entertainment = 50;
+    private String illness = "";
+    private bool fed = false;
+
+    private int day = 1;
+    private int money = 2000;
+
+
+    private int maxFeed = 20;
+    private int currFeed = 0;
+    private int hayWeight = 10;
+    private int fruitWeight = 20;
+    private int liveFoodWeight = 20;
+
+    private int inMenu = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
     {
+        dayText.text = $"Day {day}, ${money}";
+        animalNeeds.text = $"Hunger: {hunger}%\nThirst: {thirst}%\nEntertainment: {entertainment}%\nIllness: {illness}";
+    }
+
+    public void DayButtonOnClick()
+    {
+        day += 1;
+        money += 100;
+
+        hunger -= 30;
+        thirst -= 30;
+        entertainment -= 30;
+        currFeed = 0;
+        fed = false;
+    }
+
+    public void FeedButtonOnClick()
+    {
+        inMenu = 1;
+        // //hay button
+        // GameObject hayFoodButtonObject = new GameObject("HayFoodButton");
+
+        // UnityEngine.UI.Image hayButtonImage = hayFoodButtonObject.AddComponent<UnityEngine.UI.Image>();
+        // Button hayButton = hayFoodButtonObject.AddComponent<Button>();
+
+        // hayFoodButtonObject.transform.SetParent(canvas.transform, false);
+
+        // RectTransform rectTransform = hayFoodButtonObject.GetComponent<RectTransform>();
+        // rectTransform.sizeDelta = new Vector2(160, 60);
+        // rectTransform.anchoredPosition = new Vector3(200, 200, 0);
+
+        // hayButton.onClick.AddListener(HayButtonOnClick);
+
+
+        // GameObject textObject = new GameObject("Text");
+        // textObject.transform.SetParent(hayFoodButtonObject.transform, false);
+
+        // TextMeshProUGUI buttonText = textObject.AddComponent<TextMeshProUGUI>();
+        // buttonText.text = "Hay";
+        // buttonText.alignment = TextAlignmentOptions.Center;
+        // buttonText.color = Color.black;
+
+        // RectTransform textRect = textObject.GetComponent<RectTransform>();
+        // textRect.anchorMin = Vector2.zero;
+        // textRect.anchorMax = Vector2.one;
+        // textRect.offsetMin = Vector2.zero;
+        // textRect.offsetMax = Vector2.zero;
+    }
+
+    public void WaterButtonOnClick()
+    {
+        
+    }
+
+    public void EntertainmentButtonOnClick()
+    {
+        
+    }
+
+    public void HayButtonOnClick()
+    {
+        money -= 50;
+        hunger += 5;
+        currFeed += hayWeight;
+
+        if (currFeed >= maxFeed)
+        {
+            fed = true;
+        }
+
+        inMenu = 0;
+    }
+
+    public void FruitButtonOnClick()
+    {
+        money -= 50;
+        hunger += 30;
+        currFeed += fruitWeight;
+
+        if (currFeed >= maxFeed)
+        {
+            fed = true;
+        }
+
+        inMenu = 0;
+    }
+
+    public void LiveFoodButtonOnClick()
+    {
+        money -= 50;
+        hunger += 50;
+        currFeed += liveFoodWeight;
+
+        if (currFeed >= maxFeed)
+        {
+            fed = true;
+        }
+        
+        inMenu = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inMenu == 0)
+        {
+            if (fed == false && hunger < 100)
+            {
+                FeedButton.gameObject.SetActive(true);
+            }
+            
+            WaterButton.gameObject.SetActive(true);
+            EntertainmentButton.gameObject.SetActive(true);
+
+            HayButton.gameObject.SetActive(false);
+            FruitButton.gameObject.SetActive(false);
+            LiveFoodButton.gameObject.SetActive(false);
+        }
+
+        if (inMenu == 1)
+        {
+            FeedButton.gameObject.SetActive(false);
+            WaterButton.gameObject.SetActive(false);
+            EntertainmentButton.gameObject.SetActive(false);
+
+            HayButton.gameObject.SetActive(true);
+            FruitButton.gameObject.SetActive(true);
+            LiveFoodButton.gameObject.SetActive(true);
+            
+        }
+
+
+
+        hunger = System.Math.Clamp(hunger, 0, 100);
+        thirst = System.Math.Clamp(thirst, 0, 100);
+        entertainment = System.Math.Clamp(entertainment, 0, 100);
+        if (hunger < 10)
+        {
+            illness = "Starving";
+        } else
+        {
+            illness = "";
+        }
+        dayText.text = $"Day {day}, ${money}";
+        animalNeeds.text = $"Hunger: {hunger}%\nThirst: {thirst}%\nEntertainment: {entertainment}%\nIllness: {illness}";
+
+        
         
     }
 }
