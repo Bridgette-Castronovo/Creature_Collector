3127fc4ca84dda4cd7dc2beefe5e93ba3c42ffaf
twest67 <63468489+Marzipayn@users.noreply.github.com>
2026-03-02 10:57:52 -0500

    File Cleanup

--- Assets/Scripts/Creature.cs
+++ Assets/Scripts/Creature.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
 
-public class Creature : MonoBehaviour
+[CreateAssetMenu(fileName = "Creature", menuName = "Scriptable Objects/Creature")]
+public class Creature : ScriptableObject
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public string speciesName = "New Species";
+    public string speciesDesc = "Add a description for this species.";
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public int weightMin = 100;
+    public int weightMax = 200;
+
+    public int hunger = 150;
+
+    public int fruitMin = 50;
+    public int fruitMax = 100;
+
+    public int meatMin = 50;
+    public int meatMax = 100;
+
+    public int grainsMin = 50;
+    public int grainsMax = 100;
+
+    public int crystalMin = 50;
+    public int crystalMax = 100;
+
+    public bool research = false;
 }
