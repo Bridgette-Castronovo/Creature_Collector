28574c63d877ece922761ec5e935bee6adeb115e
Bridgette-Castronovo <145254750+Bridgette-Castronovo@users.noreply.github.com>
2026-04-24 08:27:14 -0400

    mini habitat

--- Assets/Scripts/CubePlacer.cs
+++ Assets/Scripts/CubePlacer.cs
@@ -5,36 +5,63 @@ public class CubePlacer : MonoBehaviour
     public Material validMaterial;
     public Material invalidMaterial;
     public AudioClip dropSound;
-    public AudioClip dragSound; // Assign in Inspector
-
+    public AudioClip dragSound;
+    public float yOffset = 0f;
     private Camera cam;
     private bool isDragging = false;
     private Vector2Int currentCell;
     private Vector2Int previousCell;
     private bool hasBeenPlaced = false;
     private int layerMask;
-    private Renderer rend;
-    private Material defaultMaterial;
+    private Renderer[] renderers;
+    private Material[] defaultMaterials;
 
     void Awake()
     {
         cam = Camera.main;
-        rend = GetComponent<Renderer>();
-        defaultMaterial = rend.material;
+        renderers = GetComponentsInChildren<Renderer>();
+
+        defaultMaterials = new Material[renderers.Length];
+        for (int i = 0; i < renderers.Length; i++)
+        {
+            defaultMaterials[i] = renderers[i].material;
+        }
+
         int cubeLayer = LayerMask.NameToLayer("Cube");
         layerMask = ~(1 << cubeLayer);
+    }
+
+    void SetMaterial(Material mat)
+    {
+        foreach (Renderer r in renderers)
+        {
+            r.material = mat;
+        }
+    }
+
+    void ResetMaterials()
+    {
+        for (int i = 0; i < renderers.Length; i++)
+        {
+            renderers[i].material = defaultMaterials[i];
+        }
+    }
+
+    Vector3 GetPlacementPosition(Vector2Int cell)
+    {
+        return GridManager.Instance.GridToWorld(cell) + Vector3.up * yOffset;
     }
 
     public void StartDragging()
     {
         isDragging = true;
-        PlayDragSound(); // For programmatic drag starts
+        PlayDragSound();
     }
 
     void OnMouseDown()
     {
         isDragging = true;
-        PlayDragSound(); // Plays when player clicks and starts dragging
+        PlayDragSound();
         if (hasBeenPlaced)
         {
             GridManager.Instance.UnregisterCell(previousCell);
@@ -72,9 +99,10 @@ public class CubePlacer : MonoBehaviour
             {
                 currentCell = GridManager.Instance.WorldToGrid(hit.point);
                 currentCell = GridManager.Instance.ClampToGrid(currentCell);
-                transform.position = GridManager.Instance.GridToWorld(currentCell);
+                transform.position = GetPlacementPosition(currentCell);
+
                 bool occupied = GridManager.Instance.IsCellOccupied(currentCell);
-                rend.material = occupied ? invalidMaterial : validMaterial;
+                SetMaterial(occupied ? invalidMaterial : validMaterial);
             }
         }
     }
@@ -86,8 +114,8 @@ public class CubePlacer : MonoBehaviour
 
         if (!GridManager.Instance.IsCellOccupied(currentCell))
         {
-            transform.position = GridManager.Instance.GridToWorld(currentCell);
-            rend.material = defaultMaterial;
+            transform.position = GetPlacementPosition(currentCell);
+            ResetMaterials();
             GridManager.Instance.RegisterCell(currentCell, gameObject);
             previousCell = currentCell;
             hasBeenPlaced = true;
@@ -95,8 +123,8 @@ public class CubePlacer : MonoBehaviour
         }
         else if (hasBeenPlaced)
         {
-            transform.position = GridManager.Instance.GridToWorld(previousCell);
-            rend.material = defaultMaterial;
+            transform.position = GetPlacementPosition(previousCell);
+            ResetMaterials();
             GridManager.Instance.RegisterCell(previousCell, gameObject);
             PlayDropSound();
         }
