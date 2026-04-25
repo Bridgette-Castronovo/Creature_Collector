cc47068a98585677c54d266595473aa14dfadc5e
Bridgette-Castronovo <145254750+Bridgette-Castronovo@users.noreply.github.com>
2026-04-25 14:31:21 -0400

    even more email updates

--- Assets/Scripts/EmailBodyDisplay.cs
+++ Assets/Scripts/EmailBodyDisplay.cs
@@ -1,12 +1,20 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EmailBodyDisplay : MonoBehaviour
 {
     public TMP_Text bodyText;
+    public Button attachmentButton;  // drag in from the scene
+    public Image attachmentImage;    // the image on that button
 
     public void ShowEmail(QuestEmail email)
     {
         bodyText.text = email.body;
+
+        bool hasAttachment = email.attachmentSprite != null;
+        attachmentButton.gameObject.SetActive(hasAttachment);
+        if (hasAttachment)
+            attachmentImage.sprite = email.attachmentSprite;
     }
 }
--- Assets/Scripts/QuestEmail.cs
+++ Assets/Scripts/QuestEmail.cs
@@ -1,13 +1,12 @@
-using System.Collections.Generic;
 using UnityEngine;
-
 [System.Serializable]
 public class QuestEmail
 {
-    public string id;           //"corporate_start", "conservation_m1"
+    public string id;
     public string sender;
     public string subject;
-    public string preview;      //short blurb shown in list
+    public string preview;
     [TextArea(4, 10)]
     public string body;
+    public Sprite attachmentSprite; // assign in Inspector, leave null if no attachment
 }
