8a4c3668f919b67367c6d94f83347f7854c629cd
twest67 <63468489+Marzipayn@users.noreply.github.com>
2026-04-29 17:25:34 -0400

    bug fixes

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -168,6 +168,8 @@ public class PlayerManager : MonoBehaviour
             {
                 animal.habitatHappiness += 5;
             }
+
+            animal.habitatHappiness = Math.Clamp(animal.habitatHappiness, 0, 100);
         }
         
     }
